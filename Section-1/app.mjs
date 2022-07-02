5b78b088921709d46e0dad41d7bbbfda9a6617b0
Milin Bhakta <milinbhakta@Milins-MacBook-Pro.local>
2022-07-02 16:23:12 -0300

    feat(Section-1): added the section-1 :sparkles

---
+++ Section-1/app.mjs
@@ -0,0 +1,12 @@
+import express from "express";
+import connectToDatabase from "./helpers.mjs";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+await connectToDatabase();
+
+app.listen(3000);
